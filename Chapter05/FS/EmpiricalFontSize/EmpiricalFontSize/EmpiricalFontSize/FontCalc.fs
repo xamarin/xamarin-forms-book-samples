8c7631940ffa9011197a62dd4e39da46e8962e1d
Charles Petzold <chape@microsoft.com>
2017-11-02 14:53:30 -0400

    F# VisualElement.GetSizeRequest --> Measure

--- Chapter05/FS/EmpiricalFontSize/EmpiricalFontSize/EmpiricalFontSize/FontCalc.fs
+++ Chapter05/FS/EmpiricalFontSize/EmpiricalFontSize/EmpiricalFontSize/FontCalc.fs
@@ -7,7 +7,7 @@ type FontCalc(label : Label, fontSize : float, containerWidth : float) =
 
     // Recalculate the size of the Label with the specified font size
     do label.FontSize <- fontSize
-    let sizeRequest = label.GetSizeRequest(containerWidth, Double.PositiveInfinity)
+    let sizeRequest = label.Measure(containerWidth, Double.PositiveInfinity)
 
     // Save the font size and the resultant Label height in public properties.
     member val FontSize = fontSize
